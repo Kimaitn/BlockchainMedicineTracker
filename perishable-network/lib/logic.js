676099a066b41c59fada1ac7dbe9d64a428f4674
Areax <actuallythisisspam@gmail.com>
2018-02-05 16:44:14 -0500

    Changes organization name.

--- perishable-network/lib/logic.js
+++ perishable-network/lib/logic.js
@@ -14,7 +14,7 @@
 
 /**
  * A shipment has been received by an importer
- * @param {org.acme.shipping.perishable.ShipmentReceived} shipmentReceived - the ShipmentReceived transaction
+ * @param {org.mat.ShipmentReceived} shipmentReceived - the ShipmentReceived transaction
  * @transaction
  */
 function payOut(shipmentReceived) {
@@ -74,20 +74,20 @@ function payOut(shipmentReceived) {
     console.log('Grower: ' + contract.grower.$identifier + ' new balance: ' + contract.grower.accountBalance);
     console.log('Importer: ' + contract.importer.$identifier + ' new balance: ' + contract.importer.accountBalance);
 
-    return getParticipantRegistry('org.acme.shipping.perishable.Grower')
+    return getParticipantRegistry('org.mat.Grower')
         .then(function (growerRegistry) {
             // update the grower's balance
             return growerRegistry.update(contract.grower);
         })
         .then(function () {
-            return getParticipantRegistry('org.acme.shipping.perishable.Importer');
+            return getParticipantRegistry('org.mat.Importer');
         })
         .then(function (importerRegistry) {
             // update the importer's balance
             return importerRegistry.update(contract.importer);
         })
         .then(function () {
-            return getAssetRegistry('org.acme.shipping.perishable.Shipment');
+            return getAssetRegistry('org.mat.Shipment');
         })
         .then(function (shipmentRegistry) {
             // update the state of the shipment
@@ -97,7 +97,7 @@ function payOut(shipmentReceived) {
 
 /**
  * A temperature reading has been received for a shipment
- * @param {org.acme.shipping.perishable.TemperatureReading} temperatureReading - the TemperatureReading transaction
+ * @param {org.mat.TemperatureReading} temperatureReading - the TemperatureReading transaction
  * @transaction
  */
 function temperatureReading(temperatureReading) {
@@ -112,7 +112,7 @@ function temperatureReading(temperatureReading) {
         shipment.temperatureReadings = [temperatureReading];
     }
 
-    return getAssetRegistry('org.acme.shipping.perishable.Shipment')
+    return getAssetRegistry('org.mat.Shipment')
         .then(function (shipmentRegistry) {
             // add the temp reading to the shipment
             return shipmentRegistry.update(shipment);
@@ -121,13 +121,13 @@ function temperatureReading(temperatureReading) {
 
 /**
  * Initialize some test assets and participants useful for running a demo.
- * @param {org.acme.shipping.perishable.SetupDemo} setupDemo - the SetupDemo transaction
+ * @param {org.mat.SetupDemo} setupDemo - the SetupDemo transaction
  * @transaction
  */
 function setupDemo(setupDemo) {
 
     var factory = getFactory();
-    var NS = 'org.acme.shipping.perishable';
+    var NS = 'org.mat';
 
     // create the grower
     var grower = factory.newResource(NS, 'Grower', 'farmer@email.com');
