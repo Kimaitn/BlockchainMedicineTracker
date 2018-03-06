912b10b9b190998ad9af9f5ac249022066182ce4
CristianPerez331 <cristian.perez331@knights.ucf.edu>
2018-03-06 00:28:35 -0500

    Connected Track Medicine Page to fake REST server

--- angular-app/src/app/track.component.ts
+++ angular-app/src/app/track.component.ts
@@ -1,5 +1,7 @@
 import { Component, AfterViewInit } from '@angular/core';
-
+import { LoginService } from './Login.service';
+import 'rxjs/add/operator/toPromise';
+import { Contract } from './models';
 	
 @Component({
   moduleId: module.id,
@@ -9,15 +11,25 @@ import { Component, AfterViewInit } from '@angular/core';
   './css/style.css',
   './css/jqueryui1.css',
   './css/jqueryui2.css',
-  './css/fontawesome.css']
+  './css/fontawesome.css'],
+  providers: [LoginService]
 })
 
 export class TrackComponent implements AfterViewInit  {
 	showMedicine: boolean;
     medicine: string;
+	medamount: number;
+	UoM: string;
+	private allItems;
+	private allContracts;
+	private errorMessage;
+	//contracts: Array<Contract>;
+	contracts;
+	contractlen: number;
 	
-	constructor(){
+	constructor(private serviceLogin:LoginService){
 	  this.showMedicine = false;
+	  this.contracts = new Array();
     }
 	
 	ngAfterViewInit() {
@@ -52,6 +64,97 @@ export class TrackComponent implements AfterViewInit  {
 	submitMedicine(medicine: any){
 		//this needs to connect to blockchain
 		this.medicine = medicine;
-		this.showMedicine = true;
+		this.loadAll(medicine);
 	}
+	
+	loadAll(medicine): Promise<any>  {
+    
+    //retrieve all residents
+		let itemsList = [];
+		return this.serviceLogin.getAllItems()
+		.toPromise()
+		.then((result) => {
+				this.errorMessage = null;
+			  result.forEach(item => {
+				itemsList.push(item);
+			  });     
+		})
+		.then(() => {
+
+		  for (let item of itemsList) {
+			//console.log("in for loop")
+			if(item.ItemType.itemTypeMedId==medicine){
+				console.log(item.ItemType.itemTypeMedId);
+				this.medamount = item.ItemType.itemTypeAmount;
+				this.UoM = item.ItemType.itemTypeUoM;
+				this.loadContracts(medicine);
+				this.showMedicine = true;
+				break;
+			}
+		  }
+
+		  this.allItems = itemsList;
+		}).catch((error) => {
+			if(error == 'Server error'){
+				this.errorMessage = "Could not connect to REST server. Please check your configuration details";
+			}
+			else if (error == '500 - Internal Server Error') {
+			  this.errorMessage = "Input error";
+			}
+			else{
+				this.errorMessage = error;
+			}
+		});
+
+	  }
+	  
+	  loadContracts(medicine): Promise<any>  {
+    
+    //retrieve all residents
+		let contractsList = [];
+		return this.serviceLogin.getAllContracts()
+		.toPromise()
+		.then((result) => {
+				this.errorMessage = null;
+			  result.forEach(item => {
+				contractsList.push(item);
+			  });     
+		})
+		.then(() => {
+
+		  for (let contract of contractsList) {
+			//console.log("in for loop")
+			//console.log("asd: "+contract);
+			//console.log(contract);
+			//console.log(contract.ItemType.itemTypeMedId);
+			//console.log(medicine);
+			if(contract.ItemType.itemTypeMedId==medicine){
+				//console.log("here")
+				if(contract.sellingBusiness.BusinessType == "Carrier")
+					contract.icon = "truck";
+				else
+					contract.icon = "flask";
+				this.contracts.push(contract);
+				//console.log("asd");
+				//console.log(contract);
+				//console.log(this.contracts);
+				//break;
+			}
+		  }
+		  //console.log(contracts);
+		  this.allContracts = contractsList;
+		  this.contractlen = 12/this.contracts.length; //will need to fix
+		}).catch((error) => {
+			if(error == 'Server error'){
+				this.errorMessage = "Could not connect to REST server. Please check your configuration details";
+			}
+			else if (error == '500 - Internal Server Error') {
+			  this.errorMessage = "Input error";
+			}
+			else{
+				this.errorMessage = error;
+			}
+		});
+
+	  }
  }
