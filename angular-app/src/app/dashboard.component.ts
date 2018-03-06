0b27fad3e986437267c4d554667ecbe2891c878b
CristianPerez331 <cristian.perez331@knights.ucf.edu>
2018-03-06 00:55:03 -0500

    Updated Login and set up dashboard to get data from fake REST server

--- angular-app/src/app/dashboard.component.ts
+++ angular-app/src/app/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, AfterViewInit } from '@angular/core';
-
+import { LoginService } from './Login.service';
+import 'rxjs/add/operator/toPromise';
+import { Contract } from './models';
+	
 	
 @Component({
   moduleId: module.id,
@@ -9,15 +12,25 @@ import { Component, AfterViewInit } from '@angular/core';
   './css/style.css',
   './css/jqueryui1.css',
   './css/jqueryui2.css',
-  './css/fontawesome.css']
+  './css/fontawesome.css'],
+  providers: [LoginService]
 })
 
 export class DashboardComponent implements AfterViewInit  {
 	medicine: string;
     business: string;
+	private allContracts;
+	private allItems;
+	private errorMessage;
+	contracts;
+	items;
 	
-	constructor(){
-	  this.business = "BusinessName";
+	constructor(private serviceLogin:LoginService){
+	  this.business = localStorage.getItem("name");
+	  this.contracts = new Array();
+	  this.items = new Array();
+	  this.loadContracts(this.business);
+	  this.loadItems(this.business);
     }
 	
 	ngAfterViewInit() {
@@ -52,5 +65,78 @@ export class DashboardComponent implements AfterViewInit  {
 
 	addNewContract(){
 	}
+	
+	loadItems(name): Promise<any>  {
+    
+    //retrieve all residents
+		let itemsList = [];
+		return this.serviceLogin.getAllItems()
+		.toPromise()
+		.then((result) => {
+				this.errorMessage = null;
+			  result.forEach(item => {
+				itemsList.push(item);
+			  });     
+		})
+		.then(() => {
+
+		  for (let item of itemsList) {
+			  //console.log(item);
+			if(item.Business==name){ //probs gunna have to fix this when actually connecting
+				this.items.push(item);
+				
+			}
+		  }
+		  //console.log(this.items);
+		  this.allItems = itemsList;
+		}).catch((error) => {
+			if(error == 'Server error'){
+				this.errorMessage = "Could not connect to REST server. Please check your configuration details";
+			}
+			else if (error == '500 - Internal Server Error') {
+			  this.errorMessage = "Input error";
+			}
+			else{
+				this.errorMessage = error;
+			}
+		});
+
+	  }
+	
+	loadContracts(name): Promise<any>  {
+    
+    //retrieve all residents
+		let contractsList = [];
+		return this.serviceLogin.getAllContracts()
+		.toPromise()
+		.then((result) => {
+				this.errorMessage = null;
+			  result.forEach(item => {
+				contractsList.push(item);
+			  });     
+		})
+		.then(() => {
+
+		  for (let contract of contractsList) {
+			if(contract.sellingBusiness.name==name||contract.buyingBusiness.name==name){
+				this.contracts.push(contract);
+				
+			}
+		  }
+		  //console.log(this.contracts);
+		  this.allContracts = contractsList;
+		}).catch((error) => {
+			if(error == 'Server error'){
+				this.errorMessage = "Could not connect to REST server. Please check your configuration details";
+			}
+			else if (error == '500 - Internal Server Error') {
+			  this.errorMessage = "Input error";
+			}
+			else{
+				this.errorMessage = error;
+			}
+		});
+
+	  }
 	
  }
