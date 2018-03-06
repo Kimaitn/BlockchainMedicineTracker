efa55343770e010138813e996dd61366b115c140
CristianPerez331 <cristian.perez331@knights.ucf.edu>
2018-03-06 00:28:35 -0500

    Connected Track Medicine Page to fake REST server

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
@@ -8,16 +9,29 @@ import { HomeComponent } from './home.component';
 import { LoginComponent } from './login.component';
 import { TrackComponent } from './track.component';
 import { DashboardComponent } from './dashboard.component';
+import { Configuration }     from './configuration';
+import { DataService }     from './data.service';
 import {routing} from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent, HomeComponent, LoginComponent, TrackComponent, DashboardComponent
+    AppComponent, 
+	HomeComponent, 
+	LoginComponent, 
+	TrackComponent, 
+	DashboardComponent
   ],
   imports: [
-    BrowserModule, FormsModule, routing
+    BrowserModule, 
+	FormsModule,
+	ReactiveFormsModule,	
+	HttpModule,
+	routing
   ],
-  providers: [],
+  providers: [
+	Configuration,
+	DataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
