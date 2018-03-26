2700958758487e1f61cbef359a0b288101dc3433
avikumar1 <avikumar137@gmail.com>
2018-03-26 13:48:59 -0700

    CSV parser in logic file wip.

--- mat/lib/logic.js
+++ mat/lib/logic.js
@@ -10,3 +10,28 @@ function tradeCommodity(itemTransaction) {
             return assetRegistry.update(itemTransaction.item);
         });
 }
+
+
+function MEDS(h1, h2, h3, h4, h5, h6){
+    this.MedNum = h1;
+    this.MedName = h2;
+    this.MedUnitOfMeasure = h3;
+    this.MedQuantity = h4;
+    this.MedCompany = h5
+    this.MedLocation = h6;
+
+}
+
+function BulkLoad(file){
+    var data = file.split("\n");
+    
+    return data.map(function (data) {
+        return data.split(",");
+    });
+
+    for(var i =0; i< data.length; i++){
+        DataStore.push(new MEDS(data[i][0], data[i][1], data[i][2], data[i][3], data[i][4], data[i][5]));
+    }
+}
+
+
