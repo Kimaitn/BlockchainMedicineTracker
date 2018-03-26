b263a6c25b14eaef6f4e6aa08206e9ca7d4922ae
Hassan <hassanmohaideen@yahoo.com>
2018-03-25 21:47:06 -0400

    updated models file in frontend
    updated the model file in the frontend to match our model file (cto) in
    the backend

--- angular-app/src/app/models.ts
+++ angular-app/src/app/models.ts
@@ -1,51 +1,104 @@
+export class Address {
+	city: string;
+	country: string;
+	street: string;
+	zip: string;
+}
+
+export class Status {
+	type: string;
+}
+
+export class BusinessType {
+	type: string;
+}
+
+export class EmployeeType {
+	type: string;
+}
+
+export class UoM {
+	type: string;
+}
+
+export class ItemType {
+	itemTypeId: string;
+	itemTypeName: string;
+	uom: UoM;
+	itemTypeMedId: number;
+}
+
+export class Item {
+	itemId: string;
+	packageType: string;
+	currentOwner: string;
+	itemType: string;
+}
+
+export class Shipment {
+	shipmentId: string;
+	status: Status;
+	itemType: string;
+	currentOwner: string;
+	destination: Address;
+	source: Address;
+	contract: string;
+	items: string[];
+}
+
+export class ItemRequest {
+	itemRequestId: string;
+	itemType: string;
+	unitPrice: number;
+	quantity: number;
+}
+
+export class Contract {
+	contractId: string;
+	status: Status;
+	requestItems: string[];
+	sellingBusiness: string;
+	buyBusiness: string;
+	shipments: string[];
+}
+
+export class Business {
+	businessId: string;
+	name: string;
+	businessType: BusinessType;
+	pocName: string;
+	pocEmail: string;
+	address: Address;
+	accountBalance: number;
+	inventory: string[];
+	employees: string[];
+}
+
+export class Employee {
+	employeeId: string;
+	firstName: string;
+	lastName: string;
+	email: string;
+	employeeType: EmployeeType;
+	phoneNumber: string;
+	worksFor: string;
+}
+
+export class ItemTransaction {
+	itemTransactionId: number;
+	newOwner: string;
+	item: string;
+}
+
+export class ShipmentTransaction {
+	shipmentTransactionId: number;
+	newOwner: string;
+	shipment: string;
+}
+
 export class Users {
 	id: number;
 	email: string;
 	password: string;
 	type: string;
-}
-
-export class Business {
-	id: number;
-	PoCEmail: string;
-	PoCPassword: string;
-	name: string;
-	BusinessType: string;
-}
-export class Item {
-	id: number;
-	packageType: string;
-	Business: string;
-	ItemType: ItemType;
-}
-
-export class ItemType {
-	id: number;
-	itemTypeName: string;
-	itemTypeAmount: number; //added this
-	itemTypeUoM: string;
-	itemTypeMedId: number;
-}
-
-export class Contract {
-	id: string;
-	status: string;
-	date: string;
-	ItemType: ItemType;
-	sellingBusiness: Business;
-	buyingBusiness: Business;
-	unitPrice: number; 
-	quantity: number;
-	Shipment: string;
-}
-
-export class Shipment {
-	shipmentId: string;
-	status: string;
-	sender: Business;
-	receiver: Business;
-	currentOwner: Business;
-	destinationAddress: string;
-	sourceAddress: string;
-	locations: string;
 }
