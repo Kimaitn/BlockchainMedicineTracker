328fbde0eb634d148e90f6dcb341477df83b2f89
Areax <actuallythisisspam@gmail.com>
2018-04-17 06:08:29 -0400

    Creates directory of files that, when deployed to multiple computers, will execute a fabric and lay the mat-network.bna on it.

---
+++ blockchain/startFabric-peer2.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Exit on first error, print all commands.
+set -e
+
+Usage() {
+	echo ""
+	echo "Usage: ./startFabric.sh [-d || --dev]"
+	echo ""
+	echo "Options:"
+	echo -e "\t-d or --dev: (Optional) enable fabric development mode"
+	echo ""
+	echo "Example: ./startFabric-peer2.sh"
+	echo ""
+	exit 1
+}
+
+Parse_Arguments() {
+	while [ $# -gt 0 ]; do
+		case $1 in
+			--help)
+				HELPINFO=true
+				;;
+            --dev | -d)
+				FABRIC_DEV_MODE=true
+				;;
+		esac
+		shift
+	done
+}
+
+Parse_Arguments $@
+
+if [ "${HELPINFO}" == "true" ]; then
+    Usage
+fi
+
+#Detect architecture
+ARCH=`uname -m`
+
+# Grab the current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ "${FABRIC_DEV_MODE}" == "true" ]; then
+    DOCKER_FILE="${DIR}"/composer/docker-compose-dev.yml
+else
+    DOCKER_FILE="${DIR}"/composer/docker-compose-peer2.yml
+fi
+
+# ARCH=$ARCH docker-compose -f "${DIR}"/composer/docker-compose-peer2.yml down
+ARCH=$ARCH docker-compose -f "${DIR}"/composer/docker-compose-peer2.yml up -d
+
+# wait for Hyperledger Fabric to start
+# incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
+export FABRIC_START_TIMEOUT=15
+echo "sleeping for ${FABRIC_START_TIMEOUT} seconds to wait for fabric to complete start up"
+sleep ${FABRIC_START_TIMEOUT}
+
+docker exec -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer2.org1.example.com peer channel fetch config -o orderer.example.com:7050 -c composerchannel
+docker exec -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer2.org1.example.com peer channel join -b composerchannel_config.block
+
+if [ "${FABRIC_DEV_MODE}" == "true" ]; then
+    echo "Fabric Network started in chaincode development mode"
+fi
