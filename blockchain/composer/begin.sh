634d02fbe81a632bfeb4b9bbb70909ad490c16ff
Hassan <hassanmohaideen@yahoo.com>
2018-04-18 19:25:40 -0400

    begin.sh

--- blockchain/composer/begin.sh
+++ blockchain/composer/begin.sh
@@ -5,3 +5,5 @@ cryptogen generate --config=./crypto-config.yaml
 export FABRIC_CFG_PATH=$PWD
 configtxgen -profile ComposerOrdererGenesis -outputBlock ./composer-genesis.block
 configtxgen -profile ComposerChannel -outputCreateChannelTx ./composer-channel.tx -channelID composerchannel
+
+
