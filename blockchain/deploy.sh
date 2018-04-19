c77151bc6cf0b833fe34a1844b319d5cb76d53f3
Areax <actuallythisisspam@gmail.com>
2018-04-19 05:35:52 -0400

    Makes edits to deploy script

--- blockchain/deploy.sh
+++ blockchain/deploy.sh
@@ -9,11 +9,11 @@ elif [ $# -gt 1 ]; then
 fi
 
 ./composer/begin.sh
-CACERT=`find ./composer/crypto-config/peerOrganizations/org1.example.com/ca/ -name '*._sk' | cut -d'/' -f2`
-KEYSTORE=`find ./composer/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/ -name '*._sk' | cut -d'/' -f2`
+CACERT=`find ./composer/crypto-config/peerOrganizations/org1.example.com/ca/ -name '*_sk' | cut -d'/' -f7`
+KEYSTORE=`find ./composer/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/ -name '*_sk' | cut -d'/' -f9`
 
 sed -i -e "s|<CA-CERT>|$CACERT|" ./composer/docker-compose.yml
-sed -i -e "s|<Peer-1-IP>|`hostname -i`|" ./composer/docker-compose.yml
+sed -i -e "s|<PEER-1-IP>|`hostname -i`|" ./composer/docker-compose.yml
 
 sed -i -e "s|<KEYSTORE>|$KEYSTORE|" ./createPeerAdminCard.sh
-sed -i -e "s|<Peer-2-IP>|$1|" ./createPeerAdminCard.sh
+sed -i -e "s|<PEER-2-IP>|$1|" ./createPeerAdminCard.sh
